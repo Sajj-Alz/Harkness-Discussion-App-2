6b76b1085898722dfc337589175b406287e36085
Sajj-Alz <sajjad.alzaher@thekaustschool.org>
2025-11-05 13:28:03 +0300

    Initial Commit

---
+++ Harkness Discussion App 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Harkness Discussion App 2
+//
+//  Created by Sajjad Alzaher on 5/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Harkness Discussion App 2Tests/Harkness_Discussion_App_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Harkness_Discussion_App_2Tests.swift
+//  Harkness Discussion App 2Tests
+//
+//  Created by Sajjad Alzaher on 5/11/25.
+//
+
+import Testing
+@testable import Harkness_Discussion_App_2
+
+struct Harkness_Discussion_App_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
